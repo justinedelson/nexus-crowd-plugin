3c033c6954a9dbe4354bf796ed9f565c80eb12b2
jedelson <jedelson@c41184c3-3edd-4803-b646-023156917d6f>
2009-01-31 22:43:27 +0000

    cleaning up search logic
    git-svn-id: https://svn.sonatype.org/nexus-plugins/trunk/nexus-crowd-auth@153 c41184c3-3edd-4803-b646-023156917d6f

--- src/main/java/org/sonatype/nexus/plugins/crowd/client/CrowdUserLocator.java
+++ src/main/java/org/sonatype/nexus/plugins/crowd/client/CrowdUserLocator.java
@@ -1,13 +1,18 @@
 package org.sonatype.nexus.plugins.crowd.client;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Configuration;
 import org.sonatype.jsecurity.locators.users.PlexusUser;
 import org.sonatype.jsecurity.locators.users.PlexusUserLocator;
+import org.sonatype.nexus.jsecurity.realms.external.crowd.CrowdAuthenticatingRealm;
 
+import com.atlassian.crowd.integration.SearchContext;
 import com.atlassian.crowd.integration.exception.InvalidAuthorizationTokenException;
 import com.atlassian.crowd.integration.exception.ObjectNotFoundException;
 import com.atlassian.crowd.integration.soap.SOAPPrincipal;
@@ -19,20 +24,32 @@ import com.google.common.collect.Sets;
 import edu.emory.mathcs.backport.java.util.Arrays;
 import edu.emory.mathcs.backport.java.util.Collections;
 
+/**
+ * Implementation of PlexusUserLocator that is backed by an instance of
+ * Atlassian Crowd.
+ * 
+ * @author Justin Edelson
+ * 
+ */
 @Component(role = PlexusUserLocator.class, hint = "Crowd")
 public class CrowdUserLocator extends BaseCrowdLocator implements PlexusUserLocator {
 
-    protected Function<SOAPPrincipal, PlexusUser> userConverter = new Function<SOAPPrincipal, PlexusUser>() {
+    /**
+     * The maximum number of results that will be returned from a user query.
+     */
+    @Configuration(value = "25")
+    private int maxResults;
 
-        public PlexusUser apply(SOAPPrincipal from) {
-            return convertToPlexusUser(from);
-        }
-    };
-
+    /**
+     * {@inheritDoc}
+     */
     public String getSource() {
         return SOURCE;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public PlexusUser getUser(String userId) {
         try {
             SOAPPrincipal principal = getCrowdClient().findPrincipalByName(userId);
@@ -48,10 +65,16 @@ public class CrowdUserLocator extends BaseCrowdLocator implements PlexusUserLoca
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public boolean isPrimary() {
-        return true;
+        return CrowdAuthenticatingRealm.isActive();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public Set<String> listUserIds() {
         try {
             String[] names = getCrowdClient().findAllPrincipalNames();
@@ -62,22 +85,41 @@ public class CrowdUserLocator extends BaseCrowdLocator implements PlexusUserLoca
         }
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     */
     public Set<PlexusUser> listUsers() {
-        try {
-            SOAPPrincipal[] principals = getCrowdClient().findAllPrincipals();
-            return Sets.newHashSet(Iterables.transform(Arrays.asList(principals), userConverter));
-
-        } catch (Exception e) {
-            getLogger().error("Unable to get userlist", e);
-            return Collections.emptySet();
-        }
+        return search("");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public Set<PlexusUser> searchUserById(String userId) {
+        return search(userId);
+    }
+
+    @SuppressWarnings("unchecked")
+    private Set<PlexusUser> search(String userId) {
+        List<SearchRestriction> searchRestrictions = new ArrayList<SearchRestriction>();
+
+        searchRestrictions.add(new SearchRestriction(SearchContext.PRINCIPAL_ACTIVE, "true"));
+        searchRestrictions.add(new SearchRestriction(SearchContext.PRINCIPAL_NAME, userId));
+        searchRestrictions.add(new SearchRestriction(SearchContext.SEARCH_INDEX_START, "1"));
+        searchRestrictions.add(new SearchRestriction(SearchContext.SEARCH_MAX_RESULTS, Integer
+                .toString(maxResults)));
+
         try {
             SOAPPrincipal[] principals = getCrowdClient().searchPrincipals(
-                    new SearchRestriction[] { new SearchRestriction("PRINCIPAL_NAME", userId) });
-            return Sets.newHashSet(Iterables.transform(Arrays.asList(principals), userConverter));
+                    searchRestrictions.toArray(new SearchRestriction[0]));
+            return Sets.newHashSet(Iterables.transform(Arrays.asList(principals),
+                    new Function<SOAPPrincipal, PlexusUser>() {
+
+                        public PlexusUser apply(SOAPPrincipal from) {
+                            return convertToPlexusUser(from);
+                        }
+                    }));
         } catch (Exception e) {
             getLogger().error("Unable to get userlist", e);
             return Collections.emptySet();
